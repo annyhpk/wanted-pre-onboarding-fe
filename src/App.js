d4d3a5791ad18d302142690b673aea51282b2a63
Chanho Kim <cksgh063@gmail.com>
2022-06-28 16:47:23 +0900

    feat: 라우터, Spiner 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import PreAssignmentGuide from './pages/PreAssignmentGuide';
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router';
+import Spiner from './components/Spiner';
+
+const MainPage = lazy(() => import('./pages/MainPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
 
 function App() {
-  return <PreAssignmentGuide />;
+  return (
+    <Suspense fallback={<Spiner />}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
